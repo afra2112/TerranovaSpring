65b09bb2e93e4dec4a281caf1d1a02fd8115eb58
afra2112 <afra65069@gmail.com>
2025-11-17 15:40:02 -0500

    arreglo imagenes para produccion

--- src/main/java/com/proyecto/terranova/config/WebConfig.java
+++ src/main/java/com/proyecto/terranova/config/WebConfig.java
@@ -13,6 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/imagenes/**")
-                .addResourceLocations("file:" + directorioImagenes);
+                .addResourceLocations("file:" + directorioImagenes)
+                .addResourceLocations(
+                        "file:" + directorioImagenes + "/",
+                        "classpath:/static/imagenes/"
+                );
     }
 }
